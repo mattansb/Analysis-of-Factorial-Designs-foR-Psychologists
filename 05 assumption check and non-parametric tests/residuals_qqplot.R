d851007a7f359a920d25a21c945b0ba14fabb3b1
mattansb <35330040+mattansb@users.noreply.github.com>
2020-02-26 00:37:55 +0200

    random effects qqplots

--- 05 assumption check and non-parametric tests/residuals_qqplot.R
+++ 05 assumption check and non-parametric tests/residuals_qqplot.R
@@ -1,32 +1,40 @@
-residuals_qqplot <- function(object){
+residuals_qqplot <- function(object, model = c("univariate","multivariate")){
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("package ggplot2 is required.", call. = FALSE)
   }
 
-  data <- object$data$long
-  id <- attr(object,'id')
-  dv <- attr(object,'dv')
+  model <- match.arg(model)
+
   within <- names(attr(object,"within"))
 
-  if (length(within)>0) {
-    formula <- as.formula(object$Anova)[-1]
-    formula <- paste0(formula, collapse = "+")
-    formula_error <- paste0("Error(",id,"/(",paste0(within,collapse = "*"),")",")")
-    formula <- paste0(c(formula,formula_error), collapse = "+")
-    formula <- as.formula(paste0(dv,"~",formula))
+  if (length(within) > 0L & model=="univariate") {
+    ## make aov object
+    data <- object$data$long
+    id <- attr(object,'id')
+    dv <- attr(object,'dv')
+    terms <- as.formula(object$Anova)[-1]
+    fixed <- paste0(terms, collapse = "+")
+    error <- paste0("Error(",id,"/(",paste0(within,collapse = "*"),")",")")
+    formula <- as.formula(paste0(dv,"~",fixed,"+",error))
     object_aov <- aov(formula,data)
 
     projections <- proj(object_aov)[-1]
+
     projections <- lapply(projections, function(.x) {
-      temp_data <- data
-      temp_data$Residuals <- as.data.frame(.x)$Residuals
-      temp_data <- unique(temp_data[,c(id,"Residuals")])
-      temp_data$Residuals
+      temp_data <- data.frame(
+        id = data[[id]],
+        resid = as.data.frame(.x)$Residuals
+      )
+      unique(temp_data)[[2]]
     })
     projections <- stack(projections, )
+  } else if (length(within) > 0L) {
+    resid <- data.frame(residuals(object$lm))
+    projections <- stack(resid)
   } else {
     projections <- data.frame(residuals(object$lm),id)
   }
+
   colnames(projections) <- c("Residuals","Error Term")
 
   # plot
