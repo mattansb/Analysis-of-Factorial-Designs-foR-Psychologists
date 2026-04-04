c1057f39f59e48112b823f56e74ef3476bab9e9e
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2026-04-04 13:19:12 +0300

    probing interactions with mfx

--- 03 interaction analysis/01 Interaction contrasts.R
+++ 03 interaction analysis/01 Interaction contrasts.R
@@ -1,6 +1,8 @@
 library(afex) # for ANOVA
 
-library(emmeans) # for follow up analysis # TODO: add {marginaleffects} examples
+# for follow up analysis
+library(emmeans)
+library(marginaleffects)
 
 afex_options(
   es_aov = 'pes',
@@ -8,7 +10,7 @@ afex_options(
 )
 
 
-# let's look at the coffee_plot.png and get a feel for our data.
+# let's look at the data/coffee_plot.png and get a feel for our data.
 #
 # What marginal (main) effects and interactions does it look like we have here?
 # What conditional (simple) effects does it look like we have here?
@@ -29,38 +31,37 @@ coffee_fit <- aov_ez(
   data = coffee_data
 )
 
-coffee_fit
-# what's up with the 3-way interaction??
-afex_plot(coffee_fit, ~time, ~coffee, ~sex)
+coffee_fit # what's up with the 3-way interaction??
 
-
-# We will be looking at the Coffee-by-Time interaction.
 
 # === NOTE ===
 # The model we used here is an ANOVA - but what follows is applicable to any
 # type of multiple regression with categorical predictors.
 
-# Explore the **simple effect** for `time` by `coffee` --------------------
-# We are looking at a 2-way interaction in a 3-way design, so this means we are
-# averaging over the levels sex.
-
+# We will be looking at the Coffee-by-Time interaction.
 afex_plot(coffee_fit, ~time, ~coffee)
 
 
-## Test simple effects ========
+# Probing the Coffee-by-Time interaction with {emmeans} ----------------------
+
+## Probing the **simple effect** for `time` by `coffee` --------------------
+# We are looking at a 2-way interaction in a 3-way design, so this means we are
+# averaging over the levels sex.
+
+### Test simple effects ---------------
 # We can break down an interaction into simple effects:
 # ("by" can be a vector for simple-simple effects, etc...)
 joint_tests(coffee_fit, by = "coffee")
 # Which rows are we looking at?
 
-## Contrast Analysis ========
+### Contrast Analysis ---------------
 
-### Step 1. Get the means --------
+# Step 1. Get the means
 em_time.coffee <- emmeans(coffee_fit, ~ time + coffee)
 em_time.coffee
 
 
-### Step 2. Compare them (conditionally) --------
+# Step 2. Compare them (conditionally)
 # We want to look at the simple effects for time, conditionally on values of
 # coffee. So we must use "by"!
 
@@ -81,7 +82,7 @@ cor(w.time)
 contrast(em_time.coffee, method = w.time, by = "coffee")
 
 
-# Follow-Up: Interaction Contrasts ----------------------------------------
+## Interaction Contrasts ----------------------------------------
 
 # After seeing the conditional contrasts - the contrasts for the effect of time
 # within the levels of coffee, we can now ask: do these contrasts DIFFER BETWEEN
@@ -107,6 +108,56 @@ contrast(em_time.coffee, interaction = list(time = w.time, coffee = "pairwise"))
 
 # Same for 4-way interactions... etc.
 
+# Probing the Coffee-by-Time interaction with {marginaleffects} ----------------
+# Let's do it all again...
+
+## Probing the **simple effect** for `time` by `coffee` --------------------
+
+### Test simple effects ---------------
+mfx <- avg_predictions(
+  coffee_fit,
+  by = c("time", "coffee"),
+  hypothesis = ~ reference | coffee
+)
+
+hypotheses(mfx, joint = 1:2) # simple time effect for coffee
+hypotheses(mfx, joint = 3:4) # simple time effect for control
+
+
+### Contrast Analysis ---------------
+
+# Compare consecutive time points for each level of coffee:
+(mfx_consec <- avg_predictions(
+  coffee_fit,
+  by = c("time", "coffee"),
+  hypothesis = ~ sequential | coffee
+))
+
+# Compare the polynomial trend for time for each level of coffee:
+avg_predictions(
+  coffee_fit,
+  by = c("time", "coffee"),
+  hypothesis = ~ poly | coffee
+)
+
+# Compare custom contrasts for time for each level of coffee:
+(mfx_custom <- avg_predictions(
+  coffee_fit,
+  by = c("time", "coffee"),
+  hypothesis = ~ I(drop(t(w.time) %*% x)) | coffee
+))
+
+
+## Interaction Contrasts ----------------------------------------
+
+# After seeing the conditional contrasts - the contrasts for the effect of time
+# within the levels of coffee, we can now ask: do these contrasts DIFFER BETWEEN
+# the levels of coffee?
+
+hypotheses(mfx_consec, hypothesis = ~ revpairwise | hypothesis)
+
+hypotheses(mfx_custom, hypothesis = ~ revpairwise | hypothesis)
+
 # Exercise ----------------------------------------------------------------
 
 # Explore the sex-by-time interaction using all the steps from above.
@@ -114,8 +165,8 @@ contrast(em_time.coffee, interaction = list(time = w.time, coffee = "pairwise"))
 # A. Which sex is the most alert in the morning?
 # B. What is the difference between noon and the afternoon for males?
 # C. Is this difference larger than the same difference for females?
-# Interpret your results along the way...
 #
+# Interpret your results along the way...
 #
 # *. Confirm (w/ contrasts, simple effects...) that there really is no 3-way
 #   interaction in the coffee data.
